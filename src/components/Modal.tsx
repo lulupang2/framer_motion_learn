f1b8c8dba1dcfd0d9cbcc4d43b66fff41ca0d74f
lulupang2 <lulupang@live.co.kr>
2023-05-17 05:52:49 +0900

    ✨ feat: card 컴포넌트 click 이벤트 처리

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,25 +1,39 @@
 import { AnimatePresence, motion, useScroll } from "framer-motion";
+import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 interface IProps {
   exampleId: string;
   children: React.ReactNode;
 }
+
 export function Modal({ exampleId, children }: IProps) {
   const { scrollY } = useScroll();
   const navigate = useNavigate();
   const onOverlayClick = () => {
     navigate("/");
   };
+  const sidebar = {
+    open: (height = 720) => ({
+      clipPath: `circle(${height * 2 + 200}px at 50% 50%)`,
+      transition: {
+        type: "spring",
+        stiffness: 20,
+        restDelta: 12,
+      },
+      transform: "translate(-50%, -50%)",
+      top: "50%",
+      left: "50%",
+    }),
+  };
   return (
     <>
-      <AnimatePresence>
-        <motion.div
-          className="Modal-Overlay"
-          onClick={onOverlayClick}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-        ></motion.div>
-      </AnimatePresence>
+      <motion.div
+        className="Modal-Overlay"
+        onClick={onOverlayClick}
+        animate={exampleId ? "open" : "closed"}
+        transition={{ duration: 1.5 }}
+        variants={sidebar}
+      ></motion.div>
       <motion.div
         className="Modal-Wrapper"
         layoutId={exampleId}
